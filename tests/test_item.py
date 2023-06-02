64cec436cfc7a6e01eecc328ed972352787b13b0
plete <pletenev_o@mail.ru>
2023-06-03 01:04:37 +0500

    Написаны тесты для класса Item

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,25 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+import pytest
+from src.item import Item
+
+@pytest.fixture
+def test_product():
+    return Item("Планшет", 12000, 10)
+
+def test_init(test_product):
+    assert test_product.name == "Планшет"
+    assert test_product.price == 12000
+    assert test_product.quantity == 10
+
+def test_calculate_total_price(test_product):
+    assert test_product.calculate_total_price() == 120000
+
+def test_apply_discount(test_product):
+    test_product.apply_discount()
+    assert test_product.price == 12000.0
+    test_product.pay_rate = 0.5
+    test_product.apply_discount()
+    assert test_product.price == 6000.0
+
+def test_all(test_product):
+    assert Item.all != []
